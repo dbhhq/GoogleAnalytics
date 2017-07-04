4e0f21fcaeb692d05601ea4b95db9e2332ec90c6
dbhhq <dbhhq@users.noreply.github.com>
2017-07-04 21:58:32 +1000

    Update app.js

--- source/app.js
+++ source/app.js
@@ -4,6 +4,27 @@
 
   if (!options.id) return
 
-  window.ga('create', options.id, domain)
-  window.ga('send', 'pageview')
+  var tbb_ga = options.id;
+  var tbb_cid = options.client_prefix;
+  var tbb_domain = 'app-apac.thebookingbutton.com';
+  ga('create', tbb_ga, {'allowLinker': true});
+  ga('require', 'linker');
+  ga('linker:autoLink', [tbb_domain], false, true);
+  ga('send', 'pageview');
+  ga(function(tracker) {
+  window.linker = window.linker || new window.gaplugins.Linker(tracker);
+  if(document.referrer.indexOf(document.domain)!=7) {
+  var e = document.createElement('iframe');
+  e.src = window.linker.decorate('//'+tbb_domain+'/'+tbb_cid+'/ga_proxy');
+  e.setAttribute('style', 'display:none');
+  var b = document.getElementsByTagName('body')[0];
+  b.appendChild(e);
+  }
+  });
+  // Automatically apply TBB prefix to thebookingbutton links
+  $(document).ready(function(){
+  $('a[href*="'+tbb_domain+'"]').each(function(){
+  this.href = this.href.replace(new RegExp(tbb_domain+'/properties', 'i'), tbb_domain+'/'+tbb_cid+'/properties');
+ });
+ });
 }())
